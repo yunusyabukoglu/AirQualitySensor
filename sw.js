8ec9acb1ce134fa363b4a89df98a2e70da681474
yunusyabukoglu <yunusyabukoglu@gmail.com>
2026-06-05 16:05:52 -0500

    Add files via upload
    Added debug console

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Bump CACHE_NAME (e.g. v1 -> v2) whenever you change index.html so phones
  * pick up the new version instead of serving the old cached copy.
  */
-const CACHE_NAME = 'aq-monitor-v7';
+const CACHE_NAME = 'aq-monitor-v8';
 const ASSETS = [
   './',
   './index.html',
