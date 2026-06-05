460b423de7a9dd9d5836458ce8dc613a0ba1f837
yunusyabukoglu <yunusyabukoglu@gmail.com>
2026-06-05 08:56:54 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Bump CACHE_NAME (e.g. v1 -> v2) whenever you change index.html so phones
  * pick up the new version instead of serving the old cached copy.
  */
-const CACHE_NAME = 'aq-monitor-v1';
+const CACHE_NAME = 'aq-monitor-v2';
 const ASSETS = [
   './',
   './index.html',
